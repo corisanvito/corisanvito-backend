2e78416d949a8dec2a0bed246ea109ede62026b1
annaficotto <ficotto.2008@gmail.com>
2026-08-21 19:17:25 +0200

    aggiunta route

--- index.js
+++ index.js
@@ -36,3 +36,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server avviato sulla porta ${PORT}`);
 });
+
+app.get('/version', (req, res) => res.json({ node: process.version }));
