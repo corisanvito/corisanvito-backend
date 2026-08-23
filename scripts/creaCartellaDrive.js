e93cbc7b7cbbc52b2a87b342b47b1965e379534c
annaficotto <ficotto.2008@gmail.com>
2026-08-23 14:12:10 +0200

    feat: add script to create Google Drive folder for Cori San Vito

---
+++ scripts/creaCartellaDrive.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { google } = require('googleapis');
+
+const auth = new google.auth.GoogleAuth({
+    credentials: {
+        client_email: process.env.GOOGLE_SERVICE_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+    },
+    scopes: ['https://www.googleapis.com/auth/drive']
+});
+
+async function creaCartella() {
+    const drive = google.drive({ version: 'v3', auth });
+
+    const res = await drive.files.create({
+        requestBody: {
+            name: 'Cori San Vito - Bacheca',
+            mimeType: 'application/vnd.google-apps.folder'
+        },
+        fields: 'id, name, webViewLink'
+    });
+
+    console.log('✅ Cartella creata!');
+    console.log('Nome:', res.data.name);
+    console.log('ID:', res.data.id);
+    console.log('Link:', res.data.webViewLink);
+    console.log('\n👉 Copia questo ID e mettilo come GOOGLE_DRIVE_FOLDER_ID su Render e nel .env');
+}
+
+creaCartella().catch(err => {
+    console.error('❌ Errore:', err.message);
+    process.exit(1);
+});
